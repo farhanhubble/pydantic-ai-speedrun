76f9e4450921b0dddbd9e403922e18944262db41
Farhan Ahmad <farhanhubble@gmail.com>
2025-12-01 12:57:17 +0800

    Async geography agent

---
+++ agents/simple_async.py
@@ -0,0 +1,32 @@
+import asyncio
+import os
+from pydantic_ai import Agent
+
+
+with open("./.secrets/gemini.key", "r") as f:
+    google_api_key = f.read().strip()
+    os.environ["GOOGLE_API_KEY"] = google_api_key
+
+agent = Agent(
+    "google-gla:gemini-2.0-flash-lite",
+)
+
+
+async def main():
+    result = await agent.run("Tell me something about the capital of Italy in roughly 140 words?")
+    print('\n', result.output)
+
+async def progress():
+    print('.', end='', flush=True)
+    await asyncio.sleep(.1)
+
+
+async def run_with_progress():
+    progress_task = asyncio.create_task(progress())
+    await main()
+    progress_task.cancel()
+
+
+if __name__ == "__main__":
+    asyncio.run(run_with_progress())
+    
